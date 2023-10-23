aadb4804a76164728951cc50fcf24383efe2f27a
Denis-isnotavailable <92397311+Denis-isnotavailable@users.noreply.github.com>
2023-10-23 12:44:24 +0300

    task-three

--- 03_cli_telegram_console_sender/index.js
+++ 03_cli_telegram_console_sender/index.js
@@ -1,1 +1,31 @@
-console.log("first")
+const TelegramBot = require('node-telegram-bot-api');
+const { Command } = require("commander");
+
+
+const TOKEN = '6701606699:AAF6UOHdSLR1xrbEkS4hmTZd5iz2qw8MWOM';
+const CHAT_ID = 449021637;
+const bot = new TelegramBot(TOKEN, { polling: true });
+const program = new Command();
+
+
+program
+    .command('send-message')
+    .description('Bot send message to the chat')
+    .argument('<message>')
+    .alias('m')
+    .action(async (msg) => {
+        await bot.sendMessage(CHAT_ID, msg);
+        process.exit();
+    });
+
+program
+    .command('send-photo')
+    .description('Bot send photo to the chat - use path to the photo')
+    .argument('<path>')
+    .alias('p')
+    .action(async (photo_path) => {        
+        await bot.sendPhoto(CHAT_ID, photo_path);
+        process.exit();
+    });
+
+program.parse();
